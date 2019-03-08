23b17f82182b4c5c385fc2872d12d4d3cebb05c0
Mike <tianhaiyang@hxxc.com>
2019-03-08 18:28:13 +0800

    添加全局守卫

--- src/main.js
+++ src/main.js
@@ -13,6 +13,19 @@ const router = new VueRouter({
   mode: 'history'
 })
 
+router.beforeEach((to, from, next) => {
+  // to and from are both route objects. must call `next`.
+  // alert('还没有登录，请先登录!')
+  // next()
+
+  if(to.path === '/login' || to.path === '/register') {
+    next()
+  }else {
+    alert('还没有登录，请先登录!')
+    next('/login')
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
